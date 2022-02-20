ec490d5504c9867d8171b0c88817218c6b7c4228
iinoto <89329951+iinoto@users.noreply.github.com>
2022-02-20 20:13:06 +0900

    Create hp.js

---
+++ hp.js
@@ -0,0 +1,13 @@
+<h1>日々のアウトプット</h1>
+<h3>私の年齢</h3>
+<ul>
+  <li>ペンネーム：iitomo</li>
+  <li>年齢：４５歳</li>
+  <li>都道府県：栃木県</li>
+</ul>
+<h3>snsへのリンク</h3>
+<ul>
+  <li><a href="https://www.facebook.com/tomomichi.iino.1">Twitter</a></li>
+</ul>
+</body>
+</html>
